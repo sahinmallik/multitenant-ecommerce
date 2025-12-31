3eee5554cec2242b42769079fe8dfaab0d232595
SAHIN MALLIK <86676300+sahinmallik@users.noreply.github.com>
2025-12-31 22:41:43 +0530

    The payload server is added

---
+++ src/app/(app)/(home)/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div className="flex items-center gap-2">
+        <p>E-HUB, INC.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
